a822b9588509530e8a0ff255723bdd48d4c7adc9
Vinayakgaikar <vinayakgaikar1998@gmail.com>
2022-11-15 11:54:54 +0530

    Create conda env and flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from distutils.log import debug
+import flask
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return "Starting inuron internship Project"
+
+if __name__=="__main__":
+    app.run(debug=True)
