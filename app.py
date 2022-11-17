b29585bc57aafe545a84ffa9460f9268501d7ccc
Vinayakgaikar <vinayakgaikar1998@gmail.com>
2022-11-17 23:03:09 +0530

    create structure of pipeline folders and create setup.py

--- app.py
+++ app.py
@@ -11,4 +11,3 @@ def index():
 if __name__=="__main__":
     app.run(debug=True)
 
-#docker build -t insurance-project:letest .
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import  setup , find_packages
+from typing import List
+
+
+#Declaring variables for setup functions
+PROJECT_NAME="insurance-premium-prediction"
+VERSION="0.0.3"
+AUTHOR="Vinayak Gaikar"
+DESCRIPTION="This is the first ineuron internship Project"
+#PACKAGES=["housing"]   # insted of this we can also use find_packeges function 
+REQUIREMENT_FILEE_NAME="requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirenents.txt file
+
+    return This function is going to return a list which contain name of 
+    libraries mentioned in requirments.txt file
+
+    """
+
+
+    with open(REQUIREMENT_FILEE_NAME) as requirement_file:
+        return requirement_file.readlines().remove("-e .")
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+packages=find_packages(),
+install_requires=get_requirements_list()
+
+)
+
+
