185e614ef026dca63d0728028a73b4afc316f4fc
Vinayakgaikar <vinayakgaikar1998@gmail.com>
2022-11-18 22:26:24 +0530

    create logger,exception,constants and config_entity.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from distutils.log import debug
+import sys
 import flask
 from flask import Flask
-
+from insurance.logger import logging
+from insurance.exception import insuranceException
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting inuron internship Project"
+    try:
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        housing = insuranceException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
+    return "Starting internship Project with CI/CD pipeline"
 
 if __name__=="__main__":
     app.run(debug=True)
